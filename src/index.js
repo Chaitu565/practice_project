dabfc0c0806beec355f7e3fc57b6ce7569f645f6
Chaitu535 <chaitanya6702@gmail.com>
2022-08-09 13:14:39 +0530

    Routing created

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import AppRouting from './AppRoute';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppRouting/>
   </React.StrictMode>
 );
 
